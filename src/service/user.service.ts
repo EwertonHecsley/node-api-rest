70f39130ac52274165002f9dd858382ab4968b1c
EwertonHecsley <ewerton.martinscomercial@gmail.com>
2023-12-10 15:52:24 -0300

    criando hash do password

--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -1,7 +1,9 @@
 import userModel from '../model/user.model';
+import bcrypt from 'bcrypt';
 
 const newUser = async (name: string, email: string, password: string) => {
-    const result = await userModel.newUser({ name, email, password });
+    const hashPassword = await bcrypt.hash(password, 8);
+    const result = await userModel.newUser({ name, email, password: hashPassword });
     return result;
 };
 
